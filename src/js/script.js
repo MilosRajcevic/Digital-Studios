8223bccc21361e33dab2a47f578f92a3e2ad8e36
Milos Rajcevic <rajcevicmilos3@gmail.com>
2022-04-19 16:32:43 +0200

    rest of pages

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,11 @@ import toggleHamburgerMenu from "./controllerMenu";
 import accordion from "./accordion";
 
 const swiper = new Swiper(".swiper", {
-  slidesPerView: 1,
+  slidesPerView: "auto",
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".js-btn-right",
+    prevEl: ".js-btn-left",
   },
 });
 
